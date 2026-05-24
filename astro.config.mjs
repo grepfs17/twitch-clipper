69c49c67c29c960f63ab7cfb9e7b5ed5349689ac
grepfs17 <grepfs17@gmail.com>
2026-05-25 01:20:21 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
+  site: "https://clipexplorer.grepfs.xyz/",
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
